37e5dd7149400c0223d1135fbe7bba54b4930d0d
anil843324 <anilkrsingh14200@gmail.com>
2025-04-17 23:18:51 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,63 +1,217 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
-
 
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
-    ChromeDriver driver;
+        ChromeDriver driver;
 
-    /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
-     */
+        /*
+         * TODO: Write your tests here with testng @Test annotation.
+         * Follow `testCase01` `testCase02`... format or what is provided in
+         * instructions
+         */
 
-     
-    /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
-     */
-    @BeforeTest
-    public void startBrowser()
-    {
-        System.setProperty("java.util.logging.config.file", "logging.properties");
+        // testCase01: Go to www.flipkart.com. Search "Washing Machine".
+        // Sort by popularity and print the count of items with rating less than or
+        // equal to 4 stars.
+        @Test(enabled = true)
+        public void testCase01() throws InterruptedException {
+                System.out.println("testCase 01 Start");
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
-        // NOT NEEDED FOR SELENIUM MANAGER
-        // WebDriverManager.chromedriver().timeout(30).setup();
+                SoftAssert softAssert = new SoftAssert();
+                // Navigate to the URL.
+                Wrappers.openURL(driver, "http://www.flipkart.com/");
 
-        ChromeOptions options = new ChromeOptions();
-        LoggingPreferences logs = new LoggingPreferences();
+                // check login Popu if present or not
+                Wrappers.closeLoginPopupIfPresent(driver, By.xpath("// div[@class='JFPqaw']/span"));
 
-        logs.enable(LogType.BROWSER, Level.ALL);
-        logs.enable(LogType.DRIVER, Level.ALL);
-        options.setCapability("goog:loggingPrefs", logs);
-        options.addArguments("--remote-allow-origins=*");
+                String currentURLString = driver.getCurrentUrl();
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+                softAssert.assertTrue(currentURLString.contains("flipkart"),
+                                "The current URL does not indicate a successful redirection to the Flipkart website.");
 
-        driver = new ChromeDriver(options);
+                // Enter Text
+                Wrappers.enterText(driver, By.xpath(
+                                "//input[starts-with(@placeholder, 'Search for Product') or @type='text' or starts-with(@title, 'Search for Product')]"),
+                                "Washing Machine");
 
-        driver.manage().window().maximize();
-    }
+                // Click on button.
+                Wrappers.clickElement(driver, By.xpath(
+                                "//button[starts-with(@aria-label, 'Search for Product') or @type='submit' or starts-with(@title, 'Search for Product')]"));
 
-    @AfterTest
-    public void endTest()
-    {
-        driver.close();
-        driver.quit();
+                wait.until(ExpectedConditions.urlContains("https://www.flipkart.com/search"));
 
-    }
+                String currentTitle = driver.getTitle();
+                boolean containsiwashingMaching = currentTitle.toLowerCase().contains("washing machine");
+                softAssert.assertTrue(containsiwashingMaching,
+                                "Search for 'washing machine' may have failed. title does not reflect the search term.");
+
+                // Click on "popularity"
+                Wrappers.clickElement(driver, By.xpath(
+                                "//div[text()='Popularity']"));
+
+                // print the count of items with rating less than or equal to 4 stars.
+
+                Thread.sleep(5000);
+                Wrappers.printItemsByRatingCondition(driver, By.xpath("//div[contains(@class,'yKfJKb')]"), By.xpath(
+                                ".//div[contains(text(), 'Washing Machine') or contains(text(), 'Automatic') or contains(text(), 'Fully') or contains(text(),'kg')]"),
+                                By.xpath(".//span[@id and contains(@id,'productRating')]"));
+
+                System.out.println("testCase01 End");
+                softAssert.assertAll();
+
+        }
+
+        // testCase02: Search "iPhone", print the Titles and discount % of items with
+        // more than 17% discount
+        @Test(enabled = true)
+        public void testCase02() throws InterruptedException {
+                System.out.println("testCase 02 Start");
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+                SoftAssert softAssert = new SoftAssert();
+                // Navigate to the URL.
+                Wrappers.openURL(driver, "http://www.flipkart.com/");
+
+                // check login Popu if present or not
+                Wrappers.closeLoginPopupIfPresent(driver, By.xpath("// div[@class='JFPqaw']/span"));
+
+                String currentURLString = driver.getCurrentUrl();
+
+                softAssert.assertTrue(currentURLString.contains("flipkart"),
+                                "The current URL does not indicate a successful redirection to the Flipkart website.");
+
+                // Enter Text "iPhone"
+                Wrappers.enterText(driver, By.xpath(
+                                "//input[starts-with(@placeholder, 'Search for Product') or @type='text' or starts-with(@title, 'Search for Product')]"),
+                                "iPhone");
+
+                // Click on search button.
+                Wrappers.clickElement(driver, By.xpath(
+                                "//button[starts-with(@aria-label, 'Search for Product') or @type='submit' or starts-with(@title, 'Search for Product')]"));
+
+                wait.until(ExpectedConditions.urlContains("https://www.flipkart.com/search"));
+
+                String currentTitle = driver.getTitle();
+                boolean containsiPhone = currentTitle.toLowerCase().contains("iphone");
+                softAssert.assertTrue(containsiPhone,
+                                "Search for 'iPhone' may have failed. title does not reflect the search term.");
+
+                // print the Titles and discount % of items with more than 17% discount
+                Thread.sleep(5000);
+                Wrappers.printItemsByDiscount(driver, By.xpath("//div[contains(@class,'yKfJKb')]"), By.xpath(
+                                ".//div[contains(text(),'Apple') or contains(text(),'iphone')]"),
+                                By.xpath(".//div[contains(@class,'UkUFwK')]/span"));
+
+                System.out.println("testCase02 End");
+                softAssert.assertAll();
+
+        }
+
+        // testCase03: Search "Coffee Mug", select 4 stars and above, and print the
+        // Title and image URL of the 5 items with highest number of reviews
+        @Test(enabled = true)
+        public void testCase03() throws InterruptedException {
+                System.out.println("testCase 03 Start");
+                SoftAssert softAssert = new SoftAssert();
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+                // Navigate to the URL.
+                Wrappers.openURL(driver, "http://www.flipkart.com/");
+
+                // check login Popu if present or not
+                Wrappers.closeLoginPopupIfPresent(driver, By.xpath("// div[@class='JFPqaw']/span"));
+
+                String currentURLString = driver.getCurrentUrl();
+
+                softAssert.assertTrue(currentURLString.contains("flipkart"),
+                                "The current URL does not indicate a successful redirection to the Flipkart website.");
+
+                // Enter Text
+                Wrappers.enterText(driver, By.xpath(
+                                "//input[starts-with(@placeholder, 'Search for Product') or @type='text' or starts-with(@title, 'Search for Product')]"),
+                                "Coffee Mug");
+
+                // Click on button.
+                Wrappers.clickElement(driver, By.xpath(
+                                "//button[starts-with(@aria-label, 'Search for Product') or @type='submit' or starts-with(@title, 'Search for Product')]"));
+
+                wait.until(ExpectedConditions.urlContains("https://www.flipkart.com/search"));
+
+                String currentTitle = driver.getTitle();
+
+                boolean containsCoffeMug = currentTitle.toLowerCase().contains("coffee mug");
+
+                softAssert.assertTrue(containsCoffeMug,
+                                "Search for 'Coffee Mug' may have failed. title does not reflect the search term.");
+
+                // select 4 stars and above
+                Wrappers.clickElement(driver, By.xpath("//div[contains(text(),'4') and contains(text(),'above')]"));
+
+                Thread.sleep(5000);
+
+                // print the Title and image URL of the 5 items with highest number of reviews
+                Wrappers.printItemByHighestReview(driver, By.xpath("//div[starts-with(@data-id, 'MUGG')]"),
+                                By.xpath(".//img[@loading='eager' or starts-with(@alt,'Pigeon ThermoCup')]"),
+                                By.xpath(".//a[@class='wjcEIp']"),
+                                By.xpath(".//span[starts-with(@id,'productRating')]/following-sibling::span"));
+
+                System.out.println("testCase03 End");
+
+                softAssert.assertAll();
+
+        }
+
+        /*
+         * Do not change the provided methods unless necessary, they will help in
+         * automation and assessment
+         */
+        @BeforeTest
+        public void startBrowser() {
+                System.setProperty("java.util.logging.config.file", "logging.properties");
+
+                // NOT NEEDED FOR SELENIUM MANAGER
+                // WebDriverManager.chromedriver().timeout(30).setup();
+
+                ChromeOptions options = new ChromeOptions();
+                LoggingPreferences logs = new LoggingPreferences();
+
+                logs.enable(LogType.BROWSER, Level.ALL);
+                logs.enable(LogType.DRIVER, Level.ALL);
+                options.setCapability("goog:loggingPrefs", logs);
+                options.addArguments("--remote-allow-origins=*");
+
+                System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
+
+                driver = new ChromeDriver(options);
+
+                driver.manage().window().maximize();
+        }
+
+        @AfterTest
+        public void endTest() {
+                driver.close();
+                driver.quit();
+
+        }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -5,18 +5,255 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
+// import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.bidi.browsingcontext.Locator;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import org.openqa.selenium.TimeoutException;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+    public static class Product {
+        String title;
+        String imageUrl;
+        int reviewCount;
+
+        // Constructor
+        public Product(String title, String imageUrl, int reviewCount) {
+            this.title = title;
+            this.imageUrl = imageUrl;
+            this.reviewCount = reviewCount;
+        }
+
+        // Getters (optional)
+        public String getTitle() {
+            return title;
+        }
+
+        public String getImageUrl() {
+            return imageUrl;
+        }
+
+        public int getReviewCount() {
+            return reviewCount;
+        }
+
+    }
+
+    public static void openURL(WebDriver driver, String url) {
+
+        try {
+            driver.get(url);
+        } catch (Exception e) {
+            System.out.println("Failed to open URL:" + url);
+            e.printStackTrace();
+        }
+    }
+
+    public static void enterText(WebDriver driver, By locator, String inputString) {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        try {
+            WebElement inputBox = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            inputBox.click();
+            inputBox.clear();
+            inputBox.sendKeys(inputString);
+        } catch (Exception e) {
+            System.out.println("Failed to enter text in input box located by: " + locator);
+            e.printStackTrace();
+        }
+    }
+
+    public static void clickElement(WebDriver driver, By locator) throws InterruptedException {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            wait.until(ExpectedConditions.elementToBeClickable(locator));
+
+            WebElement element = driver.findElement(locator);
+            try {
+                element.click();
+            } catch (Exception clickIssue) {
+                // Fallback using JavaScript
+                JavascriptExecutor js = (JavascriptExecutor) driver;
+                js.executeScript("arguments[0].click();", element);
+            }
+        } catch (Exception e) {
+            System.out.println("Failed to Click on the element located by: " + locator);
+            e.printStackTrace();
+        }
+    }
+
+    public static void printItemsByRatingCondition(WebDriver driver, By parentLocator, By titleLocator,
+            By ratingLocator) {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        int count = 1;
+        boolean found = false; // Flag to check if any product matches the condition
+
+        try {
+            List<WebElement> list = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(parentLocator));
+
+            for (int i = 0; i < list.size(); i++) {
+                String star = list.get(i).findElement(ratingLocator).getText();
+                String title = list.get(i).findElement(titleLocator).getText();
+
+                // Check if the rating is <= 4.0
+                if (Double.parseDouble(star) <= 4.0) {
+                    System.out.println(count + ". " + title + " | Rating: " + star);
+                    count++;
+                    found = true; // Set found to true when we match a product
+                }
+            }
+
+            // If no product matched the condition, print a message
+            if (!found) {
+                System.out.println("No products found with a rating of 4.0 or less.");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Failed to fetch product titles or ratings using the provided locators.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void printItemsByDiscount(WebDriver driver, By parentLocator, By titleLocator,
+            By percentageLocator) {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        int count = 1;
+        boolean found = false; // Flag to check if any product matches the condition
+
+        try {
+            List<WebElement> list = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(parentLocator));
+
+            for (int i = 0; i < list.size(); i++) {
+                try {
+                    // Try to find percentage text
+                    String percentageText = list.get(i).findElement(percentageLocator).getText().trim();
+
+                    if (percentageText.isEmpty()) {
+                        continue; // Skip if text is empty
+                    }
+
+                    String numeric = percentageText.replaceAll("[^0-9]", ""); // Extract only digits
+                    if (numeric.isEmpty()) {
+                        continue; // Skip if no number is found
+                    }
+
+                    int percentValue = Integer.parseInt(numeric);
+                    if (percentValue > 17) {
+                        String title = list.get(i).findElement(titleLocator).getText().trim();
+                        System.out.println(count + ". " + title + " | Discount: " + percentageText);
+                        count++;
+                        found = true;
+                    }
+                } catch (NoSuchElementException e) {
+                    // Skip this product if percentage element is not found
+                    continue;
+                } catch (Exception e) {
+                    System.out.println("Error processing item at index " + i + ": " + e.getMessage());
+                }
+            }
+
+            if (!found) {
+                System.out.println("No products found with a discount greater than 17%.");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Failed to fetch product titles or discounts using the provided locators.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void printItemByHighestReview(WebDriver driver, By parentLocator, By imageURLocator, By titleLocator,
+            By reviewLocator) {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        List<Product> productList = new ArrayList<>();
+
+        try {
+
+            List<WebElement> list = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(parentLocator));
+
+            for (int i = 0; i < list.size(); i++) {
+
+                try {
+
+                    // Try to find review text
+                    // reviewLocator will return in reviewText this (392)
+                    String reviewText = list.get(i).findElement(reviewLocator).getText().trim();
+
+                    if (reviewText.isEmpty()) {
+                        continue; // Skip if text is empty
+                    }
+
+                    String numeric = reviewText.replaceAll("[^0-9]", ""); // Extract only digits
+                    if (numeric.isEmpty()) {
+                        continue; // Skip if no number is found
+                    }
+
+                    int review = Integer.parseInt(numeric);
+                    String title = list.get(i).findElement(titleLocator).getText().trim();
+                    String imageURL = list.get(i).findElement(imageURLocator).getAttribute("src");
+
+                    productList.add(new Product(title, imageURL, review));
+
+                } catch (NoSuchElementException e) {
+                    // Skip this product if percentage element is not found
+                    continue;
+
+                } catch (Exception e) {
+                    System.out.println("Error processing item at index " + i + ": " + e.getMessage());
+                }
+
+            }
+
+        } catch (Exception e) {
+            System.out.println("Failed to fetch product titles or image url and review using the provided locators.");
+            e.printStackTrace();
+        }
+
+        // sort that productlist according to most number of reviews
+        productList.sort((p1, p2) -> Integer.compare(p2.getReviewCount(), p1.getReviewCount()));
+
+        // print the Title and image URL of the 5 items with highest number of reviews
+        // Math.min(5, productList.size())
+        for (int i = 0; i < 5; i++) {
+            Product p = productList.get(i);
+
+            System.out.println((i + 1) + ". " + "  | Image URL" + p.getImageUrl() + " |  Title: " + p.getTitle()
+                    + " | Reviews: " + p.getReviewCount());
+
+        }
+
+    }
+
+    public static void closeLoginPopupIfPresent(WebDriver driver, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        try {
+            WebElement loginPopup = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            loginPopup.click();
+            System.out.println("Login popup was present and clicked.");
+        } catch (TimeoutException e) {
+            System.out.println("Login popup not present. Continuing without clicking.");
+        }
+    }
+
 }
